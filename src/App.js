542a19619a7dc78a5fb7f591b546718fce1607e5
deepak7580 <mr.deepak7580@gmail.com>
2021-07-02 15:23:17 +0530

    link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <Router>
+      <div className="bg-primary text-light p-4 mb-2 d-flex justify-content-between">
+        <Link to="/home">
+          <h3 className="text-light">Home</h3>
+        </Link>
+        <Link to="/explore">
+          <h3 className="text-light">Explore</h3>
+        </Link>
+        <Link to="/notification">
+          <h3 className="text-light">Notification</h3>
+        </Link>
+        <Link to="/message">
+          <h3 className="text-light">Messages</h3>
+        </Link>
+      </div>
+
       <Route exact path="/" component={MyAppHomePage} />
       <Route exact path="/home" component={MyAppHomePage} />
       <Route exact path="/explore" component={MyAppExplorePage} />
